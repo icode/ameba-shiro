40ebe4ef031e66a7a7449d31d63ae130c6589500
icode <intelligentcodemail@gmail.com>
2015-05-14 04:04:37 +0800

    更改绑定

--- src/main/java/ameba/security/shiro/ShiroFeature.java
+++ src/main/java/ameba/security/shiro/ShiroFeature.java
@@ -2,6 +2,7 @@ package ameba.security.shiro;
 
 import ameba.security.shiro.internal.AuthInjectionBinder;
 import ameba.security.shiro.internal.ShiroDynamicFeature;
+import ameba.security.shiro.internal.ShiroExceptionMapper;
 import ameba.security.shiro.internal.SubjectFactory;
 
 import javax.ws.rs.core.Feature;
@@ -17,9 +18,10 @@ public class ShiroFeature implements Feature {
     @Override
     public boolean configure(FeatureContext context) {
         if (!context.getConfiguration().isRegistered(ShiroDynamicFeature.class)) {
-            context.register(new ShiroDynamicFeature())
-                    .register(new SubjectFactory())
-                    .register(new AuthInjectionBinder());
+            context.register(ShiroDynamicFeature.class)
+                    .register(SubjectFactory.class)
+                    .register(new AuthInjectionBinder())
+                    .register(ShiroExceptionMapper.class);
             return true;
         }
         return false;
