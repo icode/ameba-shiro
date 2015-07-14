aa30aca5f8c96a3eb7102c63673e48b3550a29a7
icode <intelligentcodemail@gmail.com>
2015-07-14 21:51:41 +0800

    更改uri判断

--- src/main/java/ameba/security/shiro/util/URIMatcher.java
+++ src/main/java/ameba/security/shiro/util/URIMatcher.java
@@ -45,9 +45,6 @@ public class URIMatcher {
                 }
             }
         }
-        if (!this.uri.startsWith("/")) {
-            this.uri = "/" + this.uri;
-        }
 
         Matcher matcher = URI_REGEX.matcher(this.uri);
         StringBuilder regex = new StringBuilder("^");
@@ -65,6 +62,9 @@ public class URIMatcher {
             this.hasFragment = this.uri.contains("#");
             this.hasQuery = this.uri.contains("\\?");
         } else {
+            if (!this.uri.startsWith("/")) {
+                this.uri = "/" + this.uri;
+            }
             this.preMatch = this.uri.endsWith("**");
             if (this.preMatch) {
                 this.uri = this.uri.substring(0, uri.length() - 3);
