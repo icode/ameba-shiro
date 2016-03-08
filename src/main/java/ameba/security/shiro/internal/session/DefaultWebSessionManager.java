7847bf12020b66540d3d1dde8287b09313f72601
icode <intelligentcodemail@gmail.com>
2016-03-08 16:31:45 +0800

    重构session，重构部分实现

--- src/main/java/ameba/security/shiro/internal/session/DefaultWebSessionManager.java
+++ src/main/java/ameba/security/shiro/internal/session/DefaultWebSessionManager.java
@@ -3,14 +3,14 @@ package ameba.security.shiro.internal.session;
 import ameba.http.session.AbstractSession;
 import org.apache.shiro.session.Session;
 import org.apache.shiro.session.SessionException;
-import org.apache.shiro.session.mgt.AbstractSessionManager;
 import org.apache.shiro.session.mgt.SessionContext;
 import org.apache.shiro.session.mgt.SessionKey;
+import org.apache.shiro.session.mgt.SessionManager;
 
 /**
  * @author icode
  */
-public class DefaultWebSessionManager extends AbstractSessionManager {
+public class DefaultWebSessionManager implements SessionManager {
 
     @Override
     public Session start(SessionContext context) {
